43a06f27d2dfc28f8ae804fb9d378b505780f5d7
Dan Doozan <dandoozan@gmail.com>
2019-03-15 22:55:16 -0700

    ConsumerReports: make openAllLinks more robust

--- src/js/contentScripts/consumerreports.js
+++ src/js/contentScripts/consumerreports.js
@@ -4,9 +4,21 @@ import './_init';
 
 add('alt+enter', 'openAllLinks', openAllLinks);
 
+function removeTrailingSlashIfNecessary(url) {
+    if (url[url.length - 1] === '/') {
+        url = url.substring(0, url.length - 1);
+    }
+    return url;
+}
+
 function openAllLinks() {
     //open links for all years
-    $('.year-details__row:visible').toArray().forEach((el) => {
-        window.open(`${window.location.href}/${$(el).attr('data-year')}`)
-    });
+    $('.year-details__row:visible')
+        .toArray()
+        .forEach(el => {
+            let windowHref = removeTrailingSlashIfNecessary(
+                window.location.href
+            );
+            window.open(`${windowHref}/${$(el).attr('data-year')}`);
+        });
 }
