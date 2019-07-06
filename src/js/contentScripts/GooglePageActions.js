79d3b47b7077665593ef39345f3d2ec9b205607b
Dan Doozan <dandoozan@gmail.com>
2019-07-05 22:45:15 -0700

    Remove jquery dependency for google

--- src/js/contentScripts/GooglePageActions.js
+++ src/js/contentScripts/GooglePageActions.js
@@ -1,16 +1,10 @@
-//todo: remove dependence on jquery
-import $ from 'jquery';
 import { PageActions } from '../PageActions';
 
-//make this class global so that I can access it in the "main" file
+//set window.Actions to this class so that I can access it in the "main" file
 window.Actions = class GooglePageActions extends PageActions {
-    //todo: move this method to PageActions
     static openAllLinks() {
-        console.log(`***openAllLinks google here2`);
-        // $('g-scrolling-carousel a')
-        //     .toArray()
-        //     .forEach(el => {
-        //         window.open($(el).attr('href'));
-        //     });
+        document.querySelectorAll('g-scrolling-carousel a').forEach(el => {
+            window.open(el.href);
+        });
     }
 };
