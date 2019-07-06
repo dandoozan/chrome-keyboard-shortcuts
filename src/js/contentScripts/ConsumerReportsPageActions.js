86a5ff5b53118c3db8a27820a2e840f017dfa06c
Dan Doozan <dandoozan@gmail.com>
2019-07-05 23:13:35 -0700

    Uncomment code in consumerreports

--- src/js/contentScripts/ConsumerReportsPageActions.js
+++ src/js/contentScripts/ConsumerReportsPageActions.js
@@ -10,28 +10,26 @@ function removeTrailingSlashIfNecessary(url) {
 }
 
 window.Actions = class ConsumerReportsPageActions extends PageActions {
-    //todo: move this method to PageActions
     static openAllLinks() {
-        console.log(`***openAllLinks consumerreports here`);
-        // let windowHref = removeTrailingSlashIfNecessary(window.location.href);
+        let windowHref = removeTrailingSlashIfNecessary(window.location.href);
 
-        // //get the current year
-        // let currYear = $(
-        //     $('div.crux-section-header.generation-range__header')[0]
-        // )
-        //     .text()
-        //     .trim();
+        //get the current year
+        let currYear = $(
+            $('div.crux-section-header.generation-range__header')[0]
+        )
+            .text()
+            .trim();
 
-        // //get all past years
-        // let pastYears = $('.year-details__row:visible')
-        //     .toArray()
-        //     .map(el => $(el).attr('data-year'));
+        //get all past years
+        let pastYears = $('.year-details__row:visible')
+            .toArray()
+            .map(el => $(el).attr('data-year'));
 
-        // let allYears = [currYear, ...pastYears];
+        let allYears = [currYear, ...pastYears];
 
-        // //generate and open links for all years
-        // allYears.forEach(year => {
-        //     window.open(`${windowHref}/${year}`);
-        // });
+        //generate and open links for all years
+        allYears.forEach(year => {
+            window.open(`${windowHref}/${year}`);
+        });
     }
 };
