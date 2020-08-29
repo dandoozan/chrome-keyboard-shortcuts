d71de173f3f1a4a00294cc1418691f280cc2efa6
Dan Doozan <dandoozan@gmail.com>
2020-08-28 21:08:41 -0700

    Add searchOnYoutube to Google

--- src/js/contentScripts/Google.js
+++ src/js/contentScripts/Google.js
@@ -7,4 +7,12 @@ export default class Google extends Page {
     //     window.open(el.href);
     // });
   }
+  static async searchOnYoutube() {
+    let searchTerm = new URL(window.location.href).searchParams.get('q');
+    window.open(
+      `https://www.youtube.com/results?search_query=${encodeURIComponent(
+        searchTerm
+      )}`
+    );
+  }
 }
