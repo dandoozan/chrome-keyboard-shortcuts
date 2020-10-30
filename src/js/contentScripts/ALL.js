f36be14cd63035dfd25dec8a96e782b72de8dd52
Dan Doozan <dandoozan@gmail.com>
2020-10-30 14:40:07 -0700

    Make all keyboard shortcuts work again

--- src/js/contentScripts/ALL.js
+++ src/js/contentScripts/ALL.js
@@ -1,28 +1,5 @@
 import { Page } from './_Page';
 
 export default class ALL extends Page {
-  static focusSearchBar() {
-    let selectors = [
-      //generics
-      'input#search',
-      'input[title="Search"]',
-      'input[type="Search"]',
-      'input[placeholder="Search"]',
-
-      'input#twotabsearchtextbox', //amazon
-      'input#global-search-input', //walmart
-      'input#search-input', //harbor freight
-    ];
-
-    let searchInput;
-    for (let i = 0; i < selectors.length; i++) {
-      searchInput = document.querySelector(selectors[i]);
-      if (searchInput) {
-        break;
-      }
-    }
-    if (searchInput) {
-      searchInput.focus();
-    }
-  }
+  //this is just a concrete class for _Page (since _Page is built differently by webpack)
 }
--- src/js/contentScripts/_Page.js
+++ src/js/contentScripts/_Page.js
@@ -1,3 +1,27 @@
 export class Page {
   //put common code for all page scripts here
+  static focusSearchBar() {
+    let selectors = [
+      //generics
+      'input#search',
+      'input[title="Search"]',
+      'input[type="Search"]',
+      'input[placeholder="Search"]',
+
+      'input#twotabsearchtextbox', //amazon
+      'input#global-search-input', //walmart
+      'input#search-input', //harbor freight
+    ];
+
+    let searchInput;
+    for (let i = 0; i < selectors.length; i++) {
+      searchInput = document.querySelector(selectors[i]);
+      if (searchInput) {
+        break;
+      }
+    }
+    if (searchInput) {
+      searchInput.focus();
+    }
+  }
 }
