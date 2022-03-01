e2728f8c709041783e8d3170205a4ce49cfe42cb
Dan Doozan <dandoozan@gmail.com>
2022-03-01 15:21:53 -0700

    DuckDuckGo: add searchOnGoogle

--- src/js/contentScripts/DuckDuckGo.js
+++ src/js/contentScripts/DuckDuckGo.js
@@ -1,6 +1,12 @@
 import { Page } from './_Page';
 
 export default class DuckDuckGo extends Page {
+  static async searchOnGoogle() {
+    let searchTerm = new URL(window.location.href).searchParams.get('q');
+    window.open(
+      `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`
+    );
+  }
   static async searchOnYoutube() {
     let searchTerm = new URL(window.location.href).searchParams.get('q');
     window.open(
