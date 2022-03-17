2de72e058fa3af465c84fd2989d2ad5cc231ae45
Dan Doozan <dandoozan@gmail.com>
2022-03-16 18:15:25 -0700

    Amazon: fix next/prev button selectors

--- src/js/contentScripts/Amazon.js
+++ src/js/contentScripts/Amazon.js
@@ -2,13 +2,21 @@ import { Page } from './_Page';
 
 export default class Amazon extends Page {
   static clickNext() {
-    let nextButton = document.querySelector('.next-button');
+    let nextButton =
+      document.querySelector('.next-button') ||
+      document.querySelector(
+        '#crReviewRow .cr-lightbox-navigator-button__next'
+      );
     if (nextButton) {
       nextButton.click();
     }
   }
   static clickPrevious() {
-    let backButton = document.querySelector('.back-button');
+    let backButton =
+      document.querySelector('.back-button') ||
+      document.querySelector(
+        '#crReviewRow .cr-lightbox-navigator-button__back'
+      );
     if (backButton) {
       backButton.click();
     }
